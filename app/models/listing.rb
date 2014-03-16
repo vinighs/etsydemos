3beabee48197245635424b8a3b1928024f1576c3
Vinicius <vinighs@gmail.com>
2014-03-15 21:58:48 -0400

    Finish setup for Dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,10 +1,14 @@
 class Listing < ActiveRecord::Base
-has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "noImage.png",
-:storage => :dropbox,
-:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	if Rails.env.development?
+	has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "noImage.png",
+	validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, :message => 'file type is not allowed (only jpeg/png/gif images)'
+	
+	else		
 
-validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, :message => 'file type is not allowed (only jpeg/png/gif images)'
- 	
- 	
-
+	has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "noImage.png",
+	:storage => :dropbox,
+	:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	:path => ":style/:id_:filename"
+	validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, :message => 'file type is not allowed (only jpeg/png/gif images)'
+end 	
 end
